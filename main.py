d32da9adfb1db901bfeb18506adb9057a7b2ff9a
BartekKownacki <barti713@live.com>
2022-05-24 13:45:49 +0200

    Mouse click handling

--- main.py
+++ main.py
@@ -1,17 +1,36 @@
+from ast import PyCF_ALLOW_TOP_LEVEL_AWAIT
+from math import floor
 import sys, pygame
 import colors as c
 
 
 pygame.init()
 
-size = width, height = 643, 483
+
 cell_size = 16
 radius = 6
 gap_size = 4
-
+grid_size_x = 32
+grid_size_y = 24 
+menu_height = 100
+button_dimensions = 80, 42
+grid_dimensions = (cell_size + gap_size)*grid_size_x, (cell_size + gap_size)*grid_size_y
+size = width, height = (grid_dimensions[0], grid_dimensions[1] + menu_height) 
 listOfCircles = []
 
+def create_grid(x, y):
+    grid = [[0 for i in range(y)] for j in range(x)]
+    return grid
+
+def draw_grid(grid):
+    for i in range(len(grid)):
+        for j in range(len(grid[i])):
+            pygame.draw.rect(screen, c.light_gray, (i * (cell_size + gap_size), j * (cell_size + gap_size), cell_size, cell_size))
+            if grid[i][j] == 1:
+                pygame.draw.circle(screen, c.black, (i * (cell_size + gap_size) + (cell_size/2), j * (cell_size + gap_size) + (cell_size/2)), radius)
+
 screen = pygame.display.set_mode(size)
+grid = create_grid(grid_size_x, grid_size_y)
 run = True
 while run:
     screen.fill(c.black)
@@ -21,13 +40,14 @@ while run:
 
         elif event.type == pygame.MOUSEBUTTONDOWN:
             x, y = pygame.mouse.get_pos()
-            listOfCircles.append((x, y))
-            # pygame.draw.circle(screen, c.red, (x, y), radius)
+            x = floor(x/(cell_size+gap_size))
+            y = floor(y/(cell_size+gap_size))
+            value = grid[x][y]
+            grid[x][y] = 1 if value == 0 else 0
+            
 
-    for x in range(gap_size, width, cell_size + gap_size):
-        for y in range(gap_size, height, cell_size + gap_size):
-            color = c.white
-            pygame.draw.rect(screen, color, (x, y, cell_size, cell_size))
+    
+    draw_grid(grid)
     for circle in listOfCircles:
         pygame.draw.circle(screen, c.red, circle, radius)
     pygame.display.update()
